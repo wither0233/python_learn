fef352ed2809b140a1237d51a8a37cdbbfd310a7
Han <767505854@qq.com>
2026-09-14 10:58:46 +0800

    condition judge exercise

---
+++ exercises/01_if.py
@@ -0,0 +1,16 @@
+# 成绩范围判断
+# grade = int(input("Enter your grade: "))
+# if grade >100 or grade <0:
+#     print("请检查你的成绩，该成绩不合法")
+# else:
+#     if grade >= 90:
+#         print("A")
+#     elif grade >= 80:
+#         print("B")
+#     elif grade >= 70:
+#         print("C")
+#     elif grade >= 60:
+#         print("D")
+#     else :
+#         print("F")
+
